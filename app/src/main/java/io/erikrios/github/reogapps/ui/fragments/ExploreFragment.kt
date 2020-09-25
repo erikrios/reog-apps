479f0c4a74eaa6dec19ff9897af90b39982d908c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-25 11:25:27 +0700

    Create TabLayoutMediator

--- app/src/main/java/io/erikrios/github/reogapps/ui/fragments/ExploreFragment.kt
+++ app/src/main/java/io/erikrios/github/reogapps/ui/fragments/ExploreFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
+import io.erikrios.github.reogapps.R
+import io.erikrios.github.reogapps.adapters.ExplorePagerAdapter
 import io.erikrios.github.reogapps.databinding.FragmentExploreBinding
 
 class ExploreFragment : Fragment() {
@@ -20,6 +23,29 @@ class ExploreFragment : Fragment() {
         _binding = FragmentExploreBinding.inflate(inflater, container, false)
 
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fragments = mutableListOf(NewsFragment(), SitesFragment(), FoodsFragment())
+        binding?.viewPager2?.adapter = ExplorePagerAdapter(this, fragments)
+
+        binding?.let {
+            binding?.tabLayout?.let { tabLayout ->
+                binding?.viewPager2?.let { viewPager2 ->
+                    TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+                        tab.apply {
+                            when (position) {
+                                0 -> text = getString(R.string.news)
+                                1 -> text = getString(R.string.sites)
+                                2 -> text = getString(R.string.foods)
+                            }
+                        }
+                    }.attach()
+                }
+            }
+        }
+
     }
 
     override fun onDestroyView() {
