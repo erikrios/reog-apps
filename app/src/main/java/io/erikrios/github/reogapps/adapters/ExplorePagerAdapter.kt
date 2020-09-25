fc5b4f39a3310d027544c0dbf872692d4a765fbd
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-09-25 11:14:52 +0700

    Change the constructor

--- app/src/main/java/io/erikrios/github/reogapps/adapters/ExplorePagerAdapter.kt
+++ app/src/main/java/io/erikrios/github/reogapps/adapters/ExplorePagerAdapter.kt
@@ -1,14 +1,13 @@
 package io.erikrios.github.reogapps.adapters
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class ExplorePagerAdapter(
-    fragmentActivity: FragmentActivity,
+    fragment: Fragment,
     val fragments: MutableList<Fragment>
 ) :
-    FragmentStateAdapter(fragmentActivity) {
+    FragmentStateAdapter(fragment) {
 
     override fun createFragment(position: Int): Fragment = fragments[position]
 
